ab644df815235f9b9bdc9207a65e3c8833cfe3ec
Chandler Freeman <chandler.freeman@gmail.com>
2015-10-31 15:56:35 -0400

    Removed useless Settings menu

--- app/src/main/java/com/defiance/chandlerfreeman/pwn/MainActivity.java
+++ app/src/main/java/com/defiance/chandlerfreeman/pwn/MainActivity.java
@@ -96,11 +96,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        // noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        else if (id == R.id.wifi_settings) {
+        if (id == R.id.wifi_settings) {
             Intent i = new Intent(getApplicationContext(), WifiSettingsActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             getApplicationContext().startActivity(i);
